9593f8d2551ea26c7cd487a375109d1e4426e6fa
Guillaume Forgue <guillaume.forgue@gmail.com>
2023-09-21 14:49:32 +0200

    fix(build): add new dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,15 @@
+type CheckVersionParams = {
+    country?: string;
+    version: string;
+    iosStoreURL?: string;
+    androidStoreURL?: string;
+};
+type CheckVersionResponse = {
+    local: string;
+    remote: string;
+    result: 'new' | 'old' | 'equal';
+    detail: 'remote > local' | 'remote < local' | 'remote === local';
+};
+export declare const compareVersion: (local: string, remote: string) => CheckVersionResponse['result'];
+declare const checkVersion: (params: CheckVersionParams) => Promise<CheckVersionResponse>;
+export default checkVersion;
--- src/android.ts
+++ src/android.ts
@@ -1,11 +1,13 @@
-const getAndroidVersion = async (storeURL: string = ''): Promise<string> => {
+export const getAndroidVersion = async (storeURL = ''): Promise<string> => {
   const appIDString = storeURL.match(/details\?id=[0-9a-zA-Z.]+/);
 
   if (!appIDString) {
     throw new Error('androidStoreURL is invalid.');
   }
 
-  const response = await fetch(`https://play.google.com/store/apps/${appIDString[0]}`).then((r) => {
+  const response = await fetch(
+    `https://play.google.com/store/apps/${appIDString[0]}`
+  ).then((r) => {
     if (r.status === 200) {
       return r.text();
     }
